77799c9d19430aa006a9c189fc52359fe5f6df9e
nabin sah <nabinsah703@gmail.com>
2022-07-31 23:59:45 +0545

    create the layout of the calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Calculator',
-      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const MyHomePage(),
     );
   }
 }
@@ -29,6 +33,64 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Simple Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  label: const Text('first number'),
+                  hintText: 'Enter the first number'),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  label: const Text('second number'),
+                  hintText: "Enter the second number"),
+            ),
+            const SizedBox(height: 8, width: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("+"),
+                ),
+                const SizedBox(height: 8, width: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("-"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8, width: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("*"),
+                ),
+                const SizedBox(height: 8, width: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("/"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
